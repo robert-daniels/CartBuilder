c1c6e2fc7f9281a84c79dd79dfb7c0c4700930ab
Robert Daniels <robertdanielsvc@gmail.com>
2023-02-16 11:08:38 -0600

    Add placeholder logo types

--- recipe-box/src/App.js
+++ recipe-box/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logos/plate_alphaLayered.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Recipe Box! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p>placeholder logo</p>
       </header>
     </div>
   );
